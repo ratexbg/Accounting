299564cef5f57f58d6390953c0d07c7cdd49d18a
ratexbg <83062383+ratexbg@users.noreply.github.com>
2021-11-15 18:24:44 +0200

    init

---
+++ Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class Account
+    {
+        private decimal balance;
+
+
+        public Account(decimal initialBalance) 
+        {
+            Balance = initialBalance;
+        }
+
+        public void Credit ( decimal amount)
+        {
+            Balance = Balance + amount;
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                if (value >= 0)
+                    balance = value;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ AccountTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class AccountTest
+    {
+        public static void Main(string[] args)
+        {
+            Account account1 = new Account(50.00M);
+            Account account2 = new Account(-7.53M);
+
+            Console.WriteLine("Account balance:{0:c}", account1.Balance);
+            Console.WriteLine("Account balance:{0:c}", account2.Balance);
+
+
+            decimal depositAmount;
+
+            Console.WriteLine("enter deposit amount for account1: ");
+            depositAmount = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("adding {0:c} to account balance\n",depositAmount);
+            account1.Credit(depositAmount);
+
+
+            Console.WriteLine("account 1 balance is {0:c}",account1.Balance);
+            Console.WriteLine("account 2 balance is {0:c}\n", account2.Balance);
+
+
+
+            Console.WriteLine("ENter deposit amount for account2: ");
+            depositAmount = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("adding {0:c} to account 2 balance\n",depositAmount);
+            account2.Credit(depositAmount);
+
+            Console.WriteLine("Account balance:{0:c}", account1.Balance);
+            Console.WriteLine("Account balance:{0:c}", account2.Balance);
+            
+
+        }
+
+    }    
+}
